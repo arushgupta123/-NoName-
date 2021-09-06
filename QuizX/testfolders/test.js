7fe37c1e688588d624ee2cc91c9569bddbdef44f
ArushGupta <71981843+arushgupta123@users.noreply.github.com>
2021-09-06 22:01:21 +0530

    Delete QuizX directory

--- QuizX/testfolders/test.js
+++
@@ -1,8 +0,0 @@
-function Submitdata() {
-    if (typeof(Storage) !== "undefined") {
-        // Store
-        localStorage.setItem("lastname", document.getElementById("d").value);
-        // Retrieve
-        document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-      }
-}
